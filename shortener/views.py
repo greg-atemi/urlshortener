c7499a8b91483c542c0b8a83a6a18c5f39318b12
Greg Atemi <56564097+greg-atemi@users.noreply.github.com>
2023-03-28 11:03:31 +0300

    Creating url shortener module
    Creating url shortener module

--- shortener/views.py
+++ shortener/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from . models import Url
+import uuid
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        url = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=url, uuid=uid)
+        new_url.save()
+
+        return HttpResponse(uid)
+
+
+def go(request, pk):
+    url_details = Url.objects.get(uuid=pk)
+    
+    return redirect(url_details.link)
