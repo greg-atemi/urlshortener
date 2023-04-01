374d58384cba87564860a11ca695bf8c28d95755
Greg Atemi <56564097+greg-atemi@users.noreply.github.com>
2023-04-01 14:35:13 +0300

    Database correction 3

--- shortener/views.py
+++ shortener/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Url
+from . models import Url
 import uuid
 
 
@@ -9,18 +9,16 @@ def index(request):
 
 
 def create(request):
-    pass
-    # if request.method == 'POST':
-    #    url = request.POST['link']
-    #    uid = str(uuid.uuid4())[:5]
-    #    new_url = Url(link=url, uuid=uid)
-    #    new_url.save()
+    if request.method == 'POST':
+        url = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=url, uuid=uid)
+        new_url.save()
 
-    #    return HttpResponse(uid)
+        return HttpResponse(uid)
 
 
 def go(request, pk):
-    pass
-    # url_details = Url.objects.get(uuid=pk)
+    url_details = Url.objects.get(uuid=pk)
 
-    # return redirect(url_details.link)
+    return redirect(url_details.link)
